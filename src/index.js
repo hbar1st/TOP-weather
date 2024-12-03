56e3245597bb3ea9d3444e000425e2c8b77eb101
hbar1st <35541470+hbar1st@users.noreply.github.com>
2024-12-03 12:38:22 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./styles.css";
+
+async function getWeather(location) {
+  try {
+    let response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?iconSet=icons2&unitGroup=metric&key=FGYFZL7WCPJ7LS7GZ4BLL4RST`
+    );
+    let weather = await response.json();
+    console.log(weather.currentConditions);
+    console.log(weather.currentConditions.conditions);
+    console.log(weather.currentConditions.precip);
+    console.log(weather.currentConditions.temp);
+    console.log(weather.currentConditions.windgust);
+    console.log(weather.currentConditions.windspeed);
+    console.log(weather.currentConditions.snow);
+    console.log(weather.currentConditions.humidity);
+    console.log(weather.currentConditions.icon);
+    console.log(weather.currentConditions.feelslike);
+  } catch (err) {
+    alert(err);
+  }
+}
+
+getWeather("london");
